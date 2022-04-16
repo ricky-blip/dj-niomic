224bda74c2cec69fd38221e05ae655f80e498a83
ricky-blip <ricky.rinaldy77@gmail.com>
2022-04-16 09:47:34 +0700

    remake

--- myproject/niomic/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class NiomicConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'niomic'
--- myproject/niomic/views.py
+++
@@ -1,7 +0,0 @@
-from django.http import HttpResponse
-
-def index(request):
-    
-    halo = "<h1> HALO DUNIA"
-    
-    return HttpResponse(halo)
